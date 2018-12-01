519e19cf27a0867c75183bbdf9015d108cc394f2
Phil Baum <phil@phib.io>
2018-12-01 14:02:45 +0100

    Part 1

---
+++ 2018/01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentFrequency = 0;
+            string line;
+
+            var fileLocation = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+
+            StreamReader inputFile = new StreamReader(Directory.GetCurrentDirectory() + @"\input.txt");  
+            while((line = inputFile.ReadLine()) != null)  
+            {  
+                int frequencyChange = int.Parse(line); 
+                currentFrequency += frequencyChange;
+            } 
+
+            Console.WriteLine(currentFrequency.ToString());
+            Console.Read();
+        }
+    }
+}
