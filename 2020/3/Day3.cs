3aef632b211a17d0b419c15d933d3034e28d54e7
Phil Baum <phil@phib.io>
2020-12-05 11:51:35 +0100

    day3 p2

--- 2020/3/Day3.cs
+++ 2020/3/Day3.cs
@@ -25,22 +25,17 @@ namespace AdventOfCode2020
 
         private void GetResultPart1()
         {
-            //  int line = 0;
             int pos = 0;
             int trees = 0;
-            int posMultiplier = 1;
 
             foreach (string checkLine in Input)
             {
-
                 List<char> chars = checkLine.ToCharArray().ToList();
 
                 int originalPos = pos;
                 if (chars.Count <= pos) //reset the pos, strings are the same
                 {
-
                     pos = pos % chars.Count;
-
                 }
 
                 if (chars[pos] == '#')
@@ -54,7 +49,53 @@ namespace AdventOfCode2020
 
         private void GetResultPart2()
         {
+            //Right 1, down 1.
+            //Right 3, down 1. (This is the slope you already checked.)           
+            //Right 5, down 1.
+            //Right 7, down 1.
+            //Right 1, down 2.
+            List<long> slopeResults = new List<long>();
+            List<Tuple<int, int>> slopes = new List<Tuple<int, int>>();
+            slopes.Add(new Tuple<int, int>(1, 1));
+            slopes.Add(new Tuple<int, int>(3, 1));
+            slopes.Add(new Tuple<int, int>(5, 1));
+            slopes.Add(new Tuple<int, int>(7, 1));
+            slopes.Add(new Tuple<int, int>(1, 2));
 
+            foreach (var slope in slopes)
+            {
+                int pos = 0;
+                int trees = 0;
+                int currentLine = 0;
+
+                foreach (string checkLine in Input)
+                {
+
+                    if (currentLine % slope.Item2 != 0)
+                    {
+                        currentLine++;
+                        continue;
+                    }
+
+                    currentLine++;
+                    List<char> chars = checkLine.ToCharArray().ToList();
+
+                    int originalPos = pos;
+                    if (chars.Count <= pos) //reset the pos, strings are the same
+                    {
+                        pos = pos % chars.Count;
+                    }
+
+                    if (chars[pos] == '#')
+                        trees++;
+
+                    pos = originalPos + slope.Item1;
+                }
+
+                slopeResults.Add((uint)trees);
+            }
+            long calc = slopeResults[0] * slopeResults[1] * slopeResults[2] * slopeResults[3] * slopeResults[4];
+            Console.Write(String.Format("Part 2 result: {0}", calc.ToString()));
         }
     }
 }
