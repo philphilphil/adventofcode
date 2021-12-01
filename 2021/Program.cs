98685e879e5e07a68800dd1bbca6b5e0e11e024e
Phil Baum <philipp.baum@mailbox.org>
2021-12-01 11:08:23 +0100

    add 2021 project

---
+++ 2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var task = new Day7();
+            task.GetResults();
+        }
+    }
+}
---
+++ 2021/TaskBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+    class TaskBase
+    {
+        public List<string> Input { get; set; }
+
+        public List<long> InputAsLong { get; set; }
+
+        public void ReadInput(int day)
+        {
+            var path = day.ToString() + "/input";
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+            Input = File.ReadAllLines(path).ToList();
+        }
+
+        public void InputToLong()
+        {
+            InputAsLong = new List<long>();
+
+            foreach (var item in Input)
+            {
+                InputAsLong.Add(long.Parse(item));
+            }
+        }
+    }
+}
