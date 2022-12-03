fcf050aa3559c9ab77131c2d05195d10ba4eebef
Phil Baum <philipp.baum@mailbox.org>
2022-12-03 08:47:57 +0100

    add template

---
+++ 2022/src/day_template.rs
@@ -0,0 +1,13 @@
+use crate::base::Problem;
+
+pub struct DayX {}
+
+impl Problem for DayX {
+    fn part_one(&self, problem_data: &ProblemData) -> String {
+        "".to_owned()
+    }
+
+    fn part_two(&self, problem_data: &ProblemData) -> String {
+        "".to_owned()
+    }
+}
