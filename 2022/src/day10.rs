2ed56a65799c6ae18fccf658aaf33abc567339bf
Phil Baum <philipp.baum@mailbox.org>
2022-12-10 20:45:41 +0100

    d10p2

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -18,8 +18,8 @@ impl Problem for Day10 {
                 Instruction::Noop => continue,
                 Instruction::AddX(number) => {
                     curr_cycle += 1;
-                    register_x += number;
                     sum_up_cycles(&mut sum_of_signals, register_x, curr_cycle);
+                    register_x += number;
                 }
             }
         }
@@ -28,7 +28,41 @@ impl Problem for Day10 {
     }
 
     fn part_two(&self, problem_data: &ProblemData) -> String {
-        "".to_owned()
+        let instructions: Vec<Instruction> = get_instructions(&problem_data.input);
+        let mut register_x = 1;
+        let mut crt_output = String::new();
+        let mut crt_position = 0;
+
+        for instruction in instructions {
+            mark_on_crt(&mut crt_output, register_x, &mut crt_position);
+            crt_position += 1;
+
+            match instruction {
+                Instruction::Noop => continue,
+                Instruction::AddX(number) => {
+                    mark_on_crt(&mut crt_output, register_x, &mut crt_position);
+                    crt_position += 1;
+                    register_x += number;
+                }
+            }
+        }
+        println!("{}", crt_output);
+
+        crt_output
+    }
+}
+
+fn mark_on_crt(crt: &mut String, register_x: i32, crt_pos: &mut i32) {
+    if *crt_pos == 40 {
+        println!("{}", crt);
+        *crt = String::new();
+        *crt_pos = 0;
+    }
+
+    if register_x == *crt_pos || register_x + 1 == *crt_pos || register_x - 1 == *crt_pos {
+        crt.push('#');
+    } else {
+        crt.push('.');
     }
 }
 
